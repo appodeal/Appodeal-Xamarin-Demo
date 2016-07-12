5f5832243d9cd7d8213505f4ab3eb40cdd206feb
Nazar Kozak <nzrkzk@gmail.com>
2016-07-13 01:46:18 +0300

    update 1.14.15

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Util;
-using System.Collections.Generic;
 using Com.Appodeal.Ads;
-using Com.Appodeal.Ads.Native_ad.Views;
 
 namespace AppodealXamarinSample
 {
 	[Activity (Label = "AppodealXamarinSample", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity, IInterstitialCallbacks, IBannerCallbacks, ISkippableVideoCallbacks, IRewardedVideoCallbacks, INativeCallbacks
+	public class MainActivity : Activity, IInterstitialCallbacks, IBannerCallbacks
 	{
 		
 		public String LOG_TAG = "Appodeal";
@@ -41,22 +36,13 @@ namespace AppodealXamarinSample
 					.SetInterests("reading, games, movies, snowboarding")
 					.SetOccupation(UserSettings.Occupation.WORK);
 
-				//Appodeal.TrackInAppPurchase(this, 100, "USD");
-				//Appodeal.DisableLocationPermissionCheck();
-				//Appodeal.DisableNetwork(this, "startapp");
-				//Appodeal.DisableNetwork(this, "startapp", Appodeal.BANNER);
-				//Appodeal.RequestAndroidMPermissions(this, this);
-				Appodeal.SetAutoCache (Appodeal.INTERSTITIAL, false);
 				Appodeal.SetTesting(false);
 				Appodeal.SetLogging(true);
 				Appodeal.SetInterstitialCallbacks(this);
 				Appodeal.SetBannerCallbacks(this);
-				Appodeal.SetSkippableVideoCallbacks(this);
-				Appodeal.SetRewardedVideoCallbacks(this);
 				Appodeal.Confirm(Appodeal.SKIPPABLE_VIDEO);
-				Appodeal.Initialize (this, "fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f", Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER | Appodeal.REWARDED_VIDEO | Appodeal.MREC | Appodeal.NATIVE);
+				Appodeal.Initialize (this, "fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f", Appodeal.INTERSTITIAL | Appodeal.BANNER);
 				Appodeal.SetBannerViewId(Resource.Id.appodealBannerView);
-				Appodeal.SetMrecViewId(Resource.Id.appodealMrecView);
 			};
 
 			Button showInterstitial = FindViewById<Button> (Resource.Id.showI);
@@ -64,26 +50,9 @@ namespace AppodealXamarinSample
 				Appodeal.Show (this, Appodeal.INTERSTITIAL);
 			};
 
-			Button showVideo = FindViewById<Button> (Resource.Id.showV);
-			showVideo.Click += delegate {
-				Appodeal.Show (this, Appodeal.SKIPPABLE_VIDEO);
-			};
-
-			Button showRewardedVideo = FindViewById<Button> (Resource.Id.rewarded);
-			showRewardedVideo.Click += delegate {
-				Appodeal.Show (this, Appodeal.REWARDED_VIDEO);
-			};
-
 			Button showBanner = FindViewById<Button> (Resource.Id.showB);
 			showBanner.Click += delegate {
-				//Appodeal.Show (this, Appodeal.BANNER_BOTTOM);
 				Appodeal.Show(this, Appodeal.BANNER_VIEW);
-			};
-
-			Button showMrec = FindViewById<Button> (Resource.Id.showM);
-			showMrec.Click += delegate {
-				Appodeal.Cache(this, Appodeal.NATIVE);
-				Appodeal.SetNativeCallbacks(this);
 			};
 
 			Button hideBanner = FindViewById<Button> (Resource.Id.hideB);
@@ -98,30 +67,10 @@ namespace AppodealXamarinSample
 		public void OnInterstitialClosed() { Log.Debug(LOG_TAG, " OnInterstitialClosed"); }
 		public void OnInterstitialClicked() { Log.Debug(LOG_TAG, " OnInterstitialClicked"); }
 
-		public void OnBannerLoaded(int height) { Log.Debug(LOG_TAG, " OnBannerLoaded"); }
+		public void OnBannerLoaded(int height, bool isPrecache) { Log.Debug(LOG_TAG, " OnBannerLoaded"); }
 		public void OnBannerFailedToLoad() { Log.Debug(LOG_TAG, " OnBannerFailedToLoad"); }
 		public void OnBannerShown() { Log.Debug(LOG_TAG, " OnBannerShown"); }
 		public void OnBannerClicked() { Log.Debug(LOG_TAG, " OnBannerClicked"); }
-
-		public void OnSkippableVideoLoaded() { Log.Debug(LOG_TAG, " OnSkippableVideoLoaded"); }
-		public void OnSkippableVideoFailedToLoad() { Log.Debug(LOG_TAG, " OnSkippableVideoFailedToLoad"); }
-		public void OnSkippableVideoShown() { Log.Debug(LOG_TAG, " OnSkippableVideoShown"); }
-		public void OnSkippableVideoClosed(bool closed) { Log.Debug(LOG_TAG, " OnSkippableVideoClosed"); }
-		public void OnSkippableVideoFinished() { Log.Debug(LOG_TAG, " OnSkippableVideoFinished"); }
-
-		public void OnRewardedVideoLoaded() { Log.Debug(LOG_TAG, " OnRewardedVideoLoaded"); }
-		public void OnRewardedVideoFailedToLoad() { Log.Debug(LOG_TAG, " OnRewardedVideoFailedToLoad"); }
-		public void OnRewardedVideoShown() { Log.Debug(LOG_TAG, " OnRewardedVideoShown"); }
-		public void OnRewardedVideoClosed(bool closed) { Log.Debug(LOG_TAG, " OnRewardedVideoClosed"); }
-		public void OnRewardedVideoFinished(int amount, String name) { Log.Debug(LOG_TAG, " OnRewardedVideoFinished " +amount); }
-
-		public void OnNativeLoaded(IList<INativeAd> nativeAds) { 
-			NativeAdViewNewsFeed nav_nf = FindViewById<NativeAdViewNewsFeed>(Resource.Id.native_ad_view_news_feed);
-			nav_nf.SetNativeAd(nativeAds[0]);
-		}
-		public void OnNativeFailedToLoad() { Log.Debug("Appodeal", "onNativeFailedToLoad"); }
-		public void OnNativeShown(INativeAd nativeAd) { Log.Debug("Appodeal", "onNativeShown"); }
-		public void OnNativeClicked(INativeAd nativeAd) { Log.Debug("Appodeal", "onNativeClicked"); }
 
 	}
 }
